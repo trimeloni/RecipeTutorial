2ef044bff942b3bdca143366c6f7efa7561a5ea5
Adam Trimeloni <atrimeloni@qsii.com>
2013-06-13 14:55:51 -0400

    cleaned up www folder. Fixed composer.json typo

---
+++ www/api/dispatch.php
@@ -0,0 +1,55 @@
+<?php
+
+$pathToProject = '../../Project';
+
+if (@include(__DIR__.$pathToProject.'/vendor/peej/tonic/src/Tonic/Autoloader.php')) { // use Tonic autoloader
+    #new Tonic\Autoloader('myNamespace'); // add another namespace
+} elseif (!@include(__DIR__.'/../vendor/autoload.php')) { // use Composer autoloader
+    die('Could not find autoloader');
+}
+
+
+
+$config = array(
+    'load' => array(
+        __DIR__.$pathToProject'/BLL/*.php'
+
+    ),
+    #'mount' => array('Tyrell' => '/nexus'), // mount in example resources at URL /nexus
+    #'cache' => new Tonic\MetadataCacheFile('/tmp/tonic.cache') // use the metadata cache
+    #'cache' => new Tonic\MetadataCacheAPC // use the metadata cache
+);
+
+$app = new Tonic\Application($config);
+
+#echo $app; die;
+
+$request = new Tonic\Request();
+
+#echo $request; die;
+
+try {
+
+    $resource = $app->getResource($request);
+
+    #echo $resource; die;
+
+    $response = $resource->exec();
+
+} catch (Tonic\NotFoundException $e) {
+    $response = new Tonic\Response(404, $e->getMessage());
+
+} catch (Tonic\UnauthorizedException $e) {
+    $response = new Tonic\Response(401, $e->getMessage());
+    $response->wwwAuthenticate = 'Basic realm="My Realm"';
+
+} catch (Tonic\Exception $e) {
+    $response = new Tonic\Response($e->getCode(), $e->getMessage());
+}
+
+#echo $response;
+
+$response->output();
+
+
+?>
